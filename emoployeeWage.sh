cb7b05c6fc956d90065e7f2d53c7905d2d899726
Rajas1312 <itsmerajas2@gmail.com>
2020-11-01 16:48:40 +0530

    A part time Employee added

--- emoployeeWage.sh
+++ emoployeeWage.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
 
 echo "welcome t0 Employees wage computation program"
+Employee_part_time=1
+Employee_full_time=2
 
 if [ $((RANDOM%2)) -eq 0 ]
 then
@@ -11,4 +13,8 @@ fi
 wage_per_hour=20
 full_day=8
 daily_wage=$((wage_per_hour*full_day))
-echo "Daily wage is" $daily_wage
+echo "Daily wage for full time employee is" $daily_wage
+
+part_time_hr=8
+
+echoo
