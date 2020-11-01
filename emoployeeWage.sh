93f8bdac82b0c56a36eab8f628faa2d9649eaef7
Rajas1312 <itsmerajas2@gmail.com>
2020-11-01 20:43:13 +0530

    program for total wage

--- emoployeeWage.sh
+++ emoployeeWage.sh
@@ -4,6 +4,10 @@ echo "welcome t0 Employees wage computation program"
 Employee_part_time=1
 Employee_full_time=2
 
+total_working_days=0
+total_wage=0
+total_working_hrs=0
+
 if [ $((RANDOM%2)) -eq 0 ]
 then
 	echo "Employee is absent"
@@ -44,3 +48,23 @@ hours(){
 }
 
 hours
+getEmployeeWage(){
+	case $1 in
+		$Employee_part_time) time1=8;;
+		$Employee_full_time) time1=8;;
+		*) time1=0;;
+	esac
+	echo $time1
+
+}
+#calculate wage for each day
+while [ $total_working_days -lt $working_days ]
+do
+	((total_working_days++))
+	time1=$( getEmployeeWage $((RANDOM%3)));
+	tempWage=$((time1*wage_per_hour));
+	total_working_hrs=$((total_working_hrs+time1));
+	total_wage=$((totalWage+tempWage));
+done
+
+echo "total wage" $total_wage
