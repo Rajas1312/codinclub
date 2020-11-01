e16838b426856f58eeaaebb0af982832aa35bab8
Rajas1312 <itsmerajas2@gmail.com>
2020-11-01 18:17:43 +0530

    calculating wages for a month

--- emoployeeWage.sh
+++ emoployeeWage.sh
@@ -16,12 +16,13 @@ daily_wage=$((wage_per_hour*full_day))
 echo "Daily wage for full time employee is" $daily_wage
 
 part_time_hr=8
+working_days=20
 
 echo "solving with switch/case statement"
 
 case $Employee_part_time in
-	$Employee_part_time) month_wage=$((daily_wage*20));;
-	$Employee_full_time) month_wage=$((daily_wage*20));;
+	$Employee_part_time) month_wage=$((daily_wage*working_days));;
+	$Employee_full_time) month_wage=$((daily_wage*working_days));;
 			*) month_wage=0;;
 esac
 	echo $month_wage;
